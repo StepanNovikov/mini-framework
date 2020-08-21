45ce57cba1b94637c8c33e4d508922c87f496ade
StepanNovikov <stepan.novikov.1996@gmail.com>
2020-08-21 23:40:20 +0300

    created spl_autoload func and added some classes

---
+++ application/core/Router.php
@@ -0,0 +1,27 @@
+<?php   
+
+    namespace application\core;     
+
+    class Router 
+    {
+        protected $routes = [];
+        protected $params = [];
+
+        function construct__(){
+            
+        }
+
+        public function add(){
+
+        }
+
+        public function match(){
+
+        }
+
+        public function run(){
+            echo 'start';
+        }
+
+        
+    }
